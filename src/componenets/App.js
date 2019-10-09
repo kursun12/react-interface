617920fdf9fde4e8747f6828b3a2d17977d9b4c4
SaliihK-PC\SaliihK <salih_kara@hotmail.nl>
2019-10-09 16:59:53 +0200

    add appoint, delete appointment added

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -5,13 +5,34 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
+import {without} from 'lodash';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       myAppointments: [],
-      lastIndex: 0
+      lastIndex: 0,
+      formDisplayBool: false,
     };
+    this.deleteAppointmentMethod = this.deleteAppointmentMethod.bind(this);
+    this.toggleFormMethod = this.toggleFormMethod.bind(this);
+
+  }
+
+  toggleFormMethod() {
+    this.setState({
+      formDisplayBool: !this.state.formDisplayBool
+    });
+  }
+
+  deleteAppointmentMethod(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
   }
 
   componentDidMount() {
@@ -38,9 +59,15 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
+                <AddAppointments
+                  formDisplay ={this.state.formDisplayBool}
+                  toggleForm={this.toggleFormMethod}
+                />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments} />
+                <ListAppointments 
+                appointments={this.state.myAppointments} // props, flowing data to the subcomponent
+                deleteAppointment={this.deleteAppointmentMethod} // receiving a event from the subcomponent
+                /> 
               </div>
             </div>
           </div>
